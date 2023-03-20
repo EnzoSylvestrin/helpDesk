2431f39f8879e116c971c33b161da55213d12e19
EnzoSylvestrin <enzospavani@gmail.com>
2023-03-20 10:07:13 -0300

    Criando componentes iniciais e o header

---
+++ src/Components/ColorComponent.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react";
+
+export const ColorComponent = () => {
+
+    const InputColor = useRef<HTMLInputElement>(null);
+
+    const HandleToggleColor = () => {
+        if (InputColor.current == null || InputColor.current.value == null) return; 
+        const Color = InputColor.current.value.toString();
+        document.body.style.setProperty('--main', Color);
+        localStorage.setItem("color", Color);
+
+        const hex = Color.replace('#', '');
+        const r = parseInt(hex.substring(0, 2), 16);
+        const g = parseInt(hex.substring(2, 4), 16);
+        const b = parseInt(hex.substring(4, 6), 16);
+      
+        const hexb = window.getComputedStyle(document.body).getPropertyValue('--bgColor').replace('#', '').replaceAll(' ', '');
+    
+        const rb = parseInt(hexb.substring(0, 2), 16);
+        const gb = parseInt(hexb.substring(2, 4), 16);
+        const bb = parseInt(hexb.substring(4, 6), 16);
+
+        const diffR = Math.abs(r - rb);
+        const diffG = Math.abs(g - gb);
+        const diffB = Math.abs(b - bb);
+        
+        if (diffR <= 25 && diffG <= 25 && diffB <= 25) {
+            //TODO change mode
+        } 
+    }
+
+    useEffect(() => {
+        if (InputColor.current == null) return; 
+        const color = localStorage.getItem("color");
+        InputColor.current.value = color || '#5f54ee';
+    }, [])
+
+    return (
+        <input 
+            type="color" 
+            className="w-6 h-6 absolute top-[calc(5vh_+_10px)] left-[60px] border-0 rounded-full pointer"
+            onInput={HandleToggleColor}
+            ref={InputColor}
+        />
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,16 @@ module.exports = {
   theme: {
     extend: {},
   },
+  fontSize: {
+    xs: 14,
+    sm: 16,
+    md: 18,
+    lg: 20,
+    xl: 24,
+    xxl: 28,
+    '2xl': '2rem',
+    xlg: '2.5rem',
+  },
+  darkMode: 'class',
   plugins: [],
 }
