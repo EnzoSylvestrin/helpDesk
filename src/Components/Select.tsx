5fde5d23f69d3ddad45f23eccca827b13163e460
EnzoSylvestrin <enzospavani@gmail.com>
2023-03-21 09:37:21 -0300

    Corrigindo erro de hydration na animação do select

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion, useIsomorphicLayoutEffect } from 'framer-motion';
 import * as Select from '@radix-ui/react-select';
 
 import { BiCheck, BiChevronDown, BiChevronUp } from 'react-icons/bi';
@@ -14,76 +14,80 @@ export type SelectProps = {
 };
 
 export const SelectComponent = ({ placeholder, items, id }: SelectProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [show, setShow] = useState(false);
 
-  const handleSelectOpen = () => {
-    setIsOpen(true);
-  };
+    const handleSelectOpen = () => {
+        setIsOpen(true);
+    };
 
-  const handleSelectClose = () => {
-    setIsOpen(false);
-  };
+    const handleSelectClose = () => {
+        setIsOpen(false);
+        console.log('fechou')
+    };
 
-  return (
-    <Select.Root>
-        <Select.Trigger
-            className="flex items-center justify-between text-sm h-full w-full bg-transparent text-darkColor dark:text-lightColor outline-none"
-            aria-label={placeholder}
-            id={id}
-            onClick={handleSelectOpen}
-        >
-            <Select.Value placeholder={placeholder} />
-            <Select.Icon>
-                <Icon icon={BiChevronDown} size={24} colored />
-            </Select.Icon>
-        </Select.Trigger>
-        <Select.Portal className="transition-all duration-500">
-            <AnimatePresence>
-                {isOpen && (
-                    <motion.div
-                        initial={{ opacity: 0, y: -50 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -50 }}
-                        className="bg-white dark:bg-slate-700 rounded-lg max-h-96 w-96 scroll-auto"
-                    >
-                        <Select.Content
-                            onCloseAutoFocus={handleSelectClose}
-                            className="max-h-96 w-96 scroll-auto"
-                        >
-                            <Select.ScrollUpButton className="flex border-t rounded-t-lg border-b-slate-300 items-center justify-center h-5 py-4 cursor-pointer">
-                                <Icon icon={BiChevronUp} size={24} colored />
-                            </Select.ScrollUpButton>
-                            <Select.Viewport>
-                                <Select.Group>
-                                    {items.map((element, i) => {
-                                        return (
-                                            <SelectItem
-                                            value={element}
-                                            className={
-                                                i === 0
-                                                ? 'rounded-t-lg'
-                                                : i === items.length - 1
-                                                ? 'rounded-b-lg'
-                                                : ''
-                                            }
-                                            key={i}
-                                            >
-                                            {FormatText(element)}
-                                            </SelectItem>
-                                        );
-                                    })}
-                                </Select.Group>
-                            </Select.Viewport>
-                            <Select.ScrollDownButton className="flex border-t rounded-b-lg border-t-slate-300 items-center justify-center h-5 py-4 bg-white cursor-pointer">
-                                <Icon icon={BiChevronDown} size={24} colored />
-                            </Select.ScrollDownButton>
-                        </Select.Content>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </Select.Portal>
-    </Select.Root>
-  );
+    useIsomorphicLayoutEffect(() => {
+        setShow(true);
+    }, []);
+
+    return (
+        <Select.Root>
+            <Select.Trigger
+                className="flex items-center justify-between text-sm h-full w-full bg-transparent text-darkColor dark:text-lightColor outline-none"
+                aria-label={placeholder}
+                id={id}
+                onClick={handleSelectOpen}
+            >
+                <Select.Value placeholder={placeholder} />
+                <Select.Icon>
+                    <Icon icon={BiChevronDown} size={24} colored />
+                </Select.Icon>
+            </Select.Trigger>
+            <motion.div
+                animate={{ 
+                    opacity: show ? isOpen ? 1 : 0 : 0,
+                }}
+                transition={{ duration: 0.4 }}
+                style={{ 
+                    visibility: show ? 'visible' : 'hidden',
+                    position: 'absolute'
+                }} // Hide the content during server-side rendering
+            >
+                <Select.Content
+                    onCloseAutoFocus={handleSelectClose}
+                    className="max-h-96 w-96 scroll-auto"
+                >
+                    <Select.ScrollUpButton className="flex border-t rounded-t-lg border-b-slate-300 items-center justify-center h-5 py-4 cursor-pointer">
+                        <Icon icon={BiChevronUp} size={24} colored />
+                    </Select.ScrollUpButton>
+                    <Select.Viewport>
+                        <Select.Group>
+                            {items.map((element, i) => {
+                            return (
+                                <SelectItem
+                                value={element}
+                                className={
+                                    i === 0
+                                    ? 'rounded-t-lg'
+                                    : i === items.length - 1
+                                    ? 'rounded-b-lg'
+                                    : ''
+                                }
+                                key={i}
+                                >
+                                {FormatText(element)}
+                                </SelectItem>
+                            );
+                            })}
+                        </Select.Group>
+                    </Select.Viewport>
+                    <Select.ScrollDownButton className="flex border-t rounded-b-lg border-t-slate-300 items-center justify-center h-5 py-4 bg-white cursor-pointer">
+                        <Icon icon={BiChevronDown} size={24} colored />
+                    </Select.ScrollDownButton>
+                </Select.Content>
+            </motion.div>
+        </Select.Root>
+    );
 };
 
 type SelectItemProps = {
@@ -100,7 +104,7 @@ const SelectItem = ({ children, value, className } : SelectItemProps) => {
                 'h-5 pl-6 px-5 py-4 relative select-none data-[disabled]:text-gray-400', 
                 'data-[disabled]:pointer-events-none data-[highlighted]:outline-none',
                 'data-[highlighted]:bg-lightHover data-[highlighted]:text-[var(--main)]',
-                'dark:text-lightColor dark:bg-darkMain',
+                'dark:text-lightColor dark:bg-darkMain dark:data-[highlighted]:bg-darkHover ',
                 className
             )}
             value={value}
