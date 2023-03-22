d3cf047eeaff7e80f14bfa51f5ebce41edae10e2
EnzoSylvestrin <enzospavani@gmail.com>
2023-03-22 17:16:24 -0300

    Retornando componente para deploy

--- src/pages/Api/GetKeys.ts
+++ src/pages/Api/GetKeys.ts
@@ -1,2 +1,8 @@
 export const API_KEY = process.env.OPENAI_API_KEY;
 export const ORG_ID = process.env.OPENAI_ORG_ID;
+
+const GetKeys = async (req: any, res: any) => {
+    res.status(200).send('');
+}
+
+export default GetKeys;
