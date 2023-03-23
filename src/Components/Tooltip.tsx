c4e2fd2125e53c80fb251ac238721ab94ffa723e
EnzoSylvestrin <enzospavani@gmail.com>
2023-03-23 15:02:03 -0300

    Retirando tooltip do portal

--- src/Components/Tooltip.tsx
+++ src/Components/Tooltip.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
-// @ts-nocheck
 import * as TooltipRadix from '@radix-ui/react-Tooltip';
 
 type TooltipProps = {
-    element: JSX.Element,
-    text: string,
-    place?: 'top' | 'bottom' | 'left' | 'right'
+  element: JSX.Element,
+  text: string,
+  place?: 'top' | 'bottom' | 'left' | 'right'
 }
 
 const Tooltip = ({ element, text, place } : TooltipProps) => {
@@ -16,18 +13,14 @@ const Tooltip = ({ element, text, place } : TooltipProps) => {
         <TooltipRadix.Trigger asChild>
           {element}
         </TooltipRadix.Trigger>
-        <TooltipRadix.Portal>
-          <div>
-            <TooltipRadix.Content
-              side={place}
-              className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-darkColor dark:text-lightColor select-none rounded-lg bg-grayMain dark:bg-darkMain p-2 text-xs leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-              sideOffset={2}
-            >
-              {text}
-              <TooltipRadix.Arrow className="fill-grayMain dark:fill-darkMain" />
-            </TooltipRadix.Content>
-          </div>
-        </TooltipRadix.Portal>
+        <TooltipRadix.Content
+          side={place}
+          className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-darkColor dark:text-lightColor select-none rounded-lg bg-InputColorLight dark:bg-InputColorDark p-2 text-xs leading-none shadow-lg will-change-[transform,opacity]"
+          sideOffset={2}
+        >
+          {text}
+          <TooltipRadix.Arrow className="fill-InputColorLight dark:fill-InputColorDark" />
+        </TooltipRadix.Content>
       </TooltipRadix.Root>
     </TooltipRadix.Provider>
   );
